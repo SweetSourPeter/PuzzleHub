ca95d71a77fa1072d9ebc692cc914628d7c7bdd7
SweetSourPeter <yaopuw@bu.edu>
2019-04-17 00:11:51 -0400

    test

---
+++ app/src/main/java/com/example/peterwang/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.peterwang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button testButton;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        testButton = findViewById(R.id.button_text);
+        textView = findViewById(R.id.textView_test);
+
+        testButton.setOnClicklistener(this);
+    }
+
+
+}

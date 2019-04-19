62a767c2418c8c2f8cf6aeadc207394355accb21
SweetSourPeter <yaopuw@bu.edu>
2019-04-19 11:58:45 -0400

    main page edited

--- app/src/main/java/com/example/peterwang/MainActivity.java
+++ app/src/main/java/com/example/peterwang/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.peterwang;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button ;
+    private Button button;
     private TextView textView;
 
     @Override
@@ -14,11 +17,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        testButton = findViewById(R.id.button_text);
-        textView = findViewById(R.id.textView_test);
-
-        testButton.setOnClicklistener(this);
+        button = findViewById(R.id.twozerofoureight);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2048();
+            }
+        });
     }
+
+        public void openActivity2048 () {
+             Intent  intent = new Intent(this, twozerofoureight.class);
+             startActivity(intent);
+        }
+
 
 
 }

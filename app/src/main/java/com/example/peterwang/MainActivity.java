c4754ba835a07fe469af3d0b9a08b748b11c0b17
SweetSourPeter <yaopuw@bu.edu>
2019-04-20 13:26:55 -0400

    main page edited

--- app/src/main/java/com/example/peterwang/MainActivity.java
+++ app/src/main/java/com/example/peterwang/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
     private TextView textView;
 
     @Override
@@ -17,20 +16,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.twozerofoureight);
-        button.setOnClickListener(new View.OnClickListener() {
+        //主界面的四个button
+        Button button1 = findViewById(R.id. gobang);
+        Button button2 = findViewById(R.id. connect);
+        Button button3 = findViewById(R.id.twozerofoureight);
+        Button button4 = findViewById(R.id. shikaku);
+
+        //四个activity的开关
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openActivity2048();
+                Intent intent = new Intent(MainActivity.this, gobang.class);
+                startActivity(intent);
             }
         });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                  Intent intent = new Intent(MainActivity.this, connect.class);
+                  startActivity(intent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, twozerofoureight.class);
+                startActivity(intent);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, shikaku.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
-
-        public void openActivity2048 () {
-             Intent  intent = new Intent(this, twozerofoureight.class);
-             startActivity(intent);
-        }
-
 
 
 }
